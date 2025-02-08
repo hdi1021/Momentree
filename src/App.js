748f27ab5445f61fad68326bc876b1ebd3eedd1c
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-02-08 15:53:29 +0900

    build :: package.json navigation add

--- src/App.js
+++ src/App.js
@@ -1,86 +1,18 @@
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './pages/Main';
 
-module.exports = {
-  project: {
-    ios: {},
-    android: {}, 
-  },
-  assets: ['./assets/fonts'], 
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Main'>
+        <Stack.Screen name="Main" component={Main}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-
-
-export default function App() {
-  return (
-    <View style={{flex:1}}>
-      <View style={styles.Header}>
-        <View style={styles.coin}>
-          <Image source={require('/Users/dgsw05/Momentree/assets/start.png')} style={styles.star}/> 
-          <Text style={styles.coinCount}>0</Text>
-        </View>
-        <Image source={require('/Users/dgsw05/Momentree/assets/ search.png')} style={styles.search}/>
-        <Image source={require('/Users/dgsw05/Momentree/assets/alarm.png')} style={styles.alram}/>
-      </View>
-
-      <View style={styles.body}>
-        <Text>2025년 2월 6일</Text>
-      </View>
-      <View style={styles.footer}></View>
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  Header:{
-    flex:1,
-    backgroundColor: "#FFFFFF",
-  },
-  body:{
-    flex:6.5,
-    backgroundColor: "#FAFAFA",
-  },
-  footer:{
-    flex:0.8,
-
-  },
-  coin:{
-    width:'15%',
-    height:'25%',
-    backgroundColor:"#C8F4ED",
-    borderRadius : 50,
-    justifyContent:"felx-start",
-    marginTop:50,
-    marginLeft:20
-  },
-  star:{
-    width: "55%",
-    height: "90%",
-    marginTop:1,
-    resizeMode: "contain",
-
-  },
-  coinCount:{
-    position: "absolute",
-    marginLeft: 35,
-    marginTop:2,
-    fontWeight:700,
-    fontSize:20,
-  },
-  search:{
-    width:'18%',
-    height:'18%',
-    marginLeft:265,
-    marginTop: -20,
-    resizeMode: "contain",
-    
-  },
-  alram:{
-    width:'18%',
-    height:'20%',
-    marginLeft:310,
-    marginTop: -20,
-    resizeMode: "contain",
-  },
-
-});
+export default App;
