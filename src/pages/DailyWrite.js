0716d1a103ce4e9441743dd6a4cfa00394d81e4d
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-02-12 20:48:13 +0900

    "build :: react-calendar add"

--- src/pages/DailyWrite.js
+++ src/pages/DailyWrite.js
@@ -1,86 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import { StyleSheet, Text, View ,Image, TextInput, Touchable} from 'react-native';
 
-
-module.exports = {
-    assets: ['../assets/fonts/'],
-  };
-  
 const DailyWrite = ({route,navigation}) => {
 
   console.log("start")
 
   return (
     <View style={{flex:1}}>
-      <View style={styles.Header}>
-        <View style={styles.coin}>
-          <Image source={require("../assets/start.png")} style={styles.star}/> 
-          <Text style={styles.coinCount}>0</Text>
-        </View>
-        <Image source={require('../assets/search.png')} style={styles.search}/>
-        <Image source={require('../assets/alarm.png')} style={styles.alram}/>
+      <View style={styles.header}>
+        <Image/>
+        <Text>2024년 2월 11일</Text>
       </View>
-
-      <View style={styles.body}>
-        <Text>2025년 2월 6일</Text>
+      <View >
+        <Touchable>
+          <Image />
+        </Touchable>
       </View>
-      <View style={styles.footer}></View>
-
+      <View>
+        <TextInput/>
+      </View>
+      <View>
+        <Touchable/>
+        <Touchable/>
+        <Touchable/>
+        <Touchable/>
+        <Touchable/>
+        <Touchable/>
+      </View>
     </View>
   );
 }
 export default DailyWrite;
 
 const styles = StyleSheet.create({
-  Header:{
-    flex:1,
-    backgroundColor: "#FFFFFF",
+  header : {
+    flex : 1
   },
-  body:{
-    flex:6.5,
-    backgroundColor: "#FAFAFA",
+  imojiView : {
+    flex : 2,
+    justifyContent : "center"
   },
-  footer:{
-    flex:0.8,
+  imoji:{
 
-  },
-  coin:{
-    width:'15%',
-    height:'25%',
-    backgroundColor:"#C8F4ED",
-    borderRadius : 50,
-    justifyContent:"flex-start",
-    marginTop:50,
-    marginLeft:20
-  },
-  star:{
-    width: "55%",
-    height: "90%",
-    marginTop:1,
-    resizeMode: "contain",
-
-  },
-  coinCount:{
-    position: "absolute",
-    marginLeft: 35,
-    marginTop:2,
-    fontWeight:700,
-    fontSize:20,
-  },
-  search:{
-    width:'18%',
-    height:'18%',
-    marginLeft:265,
-    marginTop: -20,
-    resizeMode: "contain",
-    
-  },
-  alram:{
-    width:'18%',
-    height:'20%',
-    marginLeft:310,
-    marginTop: -20,
-    resizeMode: "contain",
-  },
+  }
 
 });
